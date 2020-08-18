03840be033fb5db0fb6b3acbdf30e1a1c5fa3d38
Hassan <hassanalihazaraa@gmail.com>
2020-08-18 21:33:37 +0200

    all complete

---
+++ src/Twig/TwigExtension.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class TwigExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('Quotes', [$this, 'generateQuotes'])
+        ];
+    }
+
+    public function generateQuotes(): string
+    {
+        $quotes = ["“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+― Albert Einstein",
+            "“Darkness cannot drive out darkness,
+    only light can do that. 
+    Hate cannot drive out hate, 
+    only love can do that.”
+― Martin Luther King Jr., A Testament of Hope: The Essential Writings and Speeches ",
+            "“Be the change that you wish to see in the world.”
+― Mahatma Gandhi",
+            "“If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.”
+― J.K. Rowling, Harry Potter and the Goblet of Fire",
+            "“Use lots of quotation signs, they make you look wiser.”
+    - Koen Eelen"];
+
+        return $quotes[array_rand($quotes)];
+    }
+}
